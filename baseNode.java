b5053f55eacac133d7727363d7149786214d7a5e
augustine <113469946+sthobvious@users.noreply.github.com>
2024-12-09 19:06:09 -0500

    Add files via upload

--- baseNode.java
+++ baseNode.java
@@ -1,9 +1,16 @@
 public abstract class baseNode {
-    protected int depth;
-    public int feature;
+    private int depth;
 
-    public baseNode() {
+    public baseNode() { //empty constructor
         depth = 0;
     }
-    
+
+    //set depth
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+    //get depth
+    public int getDepth() {
+        return depth;
+    }
 }
