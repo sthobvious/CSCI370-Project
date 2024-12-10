469517b107930550eb6e19b8bc7bd221e8f18aea
Fabio <71391866+Fabio858@users.noreply.github.com>
2024-12-10 17:25:29 -0500

    Update mainGUI.java

--- mainGUI.java
+++ mainGUI.java
@@ -4,32 +4,37 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 public class mainGUI extends JFrame implements ActionListener{
-	
+	JPanel panel[] = new JPanel[6]; 
+	JButton options[] = new JButton[6];
+	Container contentPane = this.getContentPane();
 	public mainGUI(String title, int height, int width) {
 		//make the actual jframe
 		setTitle(title);
 		setSize(height, width);
 		setLocation(400, 400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		Container contentPane = this.getContentPane();
-		contentPane.setLayout(new GridLayout());
+		contentPane.setLayout(new GridLayout(6, 1));
 		
-		//and relevant GUI components
-		JPanel panel[] = new JPanel[2]; 
-		JButton options[] = new JButton[2];
-
-		//buttons for admin vs user
-		panel[0] = new JPanel();
-		panel[1] = new JPanel();
+		//main menu buttons, first 2 buttons should be visible other 4 no. 
 		options[0] = new JButton("Patient Sleep Disorder Survey");
 		options[1] = new JButton("Edit Patient Database");
-		contentPane.add(panel[0]);
-		contentPane.add(panel[1]);
-		panel[0].add(options[0]);
-		panel[1].add(options[1]);
-		options[0].addActionListener(this);
-		options[1].addActionListener(this);
+		options[2] = new JButton("Remove Patient");
+		options[3] = new JButton("Add Patient");
+		options[4] = new JButton("Edit Existing Patient");
+		options[5] = new JButton("Return to Main Menu");
+		for (int i = 0; i < panel.length; i++) {
+			panel[i] = new JPanel();
+			contentPane.add(panel[i]);
+			panel[i].add(options[i]);
+			options[i].addActionListener(this);
+		}
+		
 		setVisible(true);
+		options[2].setVisible(false);
+		options[3].setVisible(false);
+		options[4].setVisible(false);
+		options[5].setVisible(false);
+		
 	}
 	
 	public void actionPerformed(ActionEvent event){
@@ -39,7 +44,30 @@ public class mainGUI extends JFrame implements ActionListener{
     		new surveyGUI("Patient Survey", 600, 850);
     	} 
     	else if (buttonName.equals("Edit Patient Database")) {      
-    		new addPatientGUI("Database Editor", 600, 950);
+    		options[0].setVisible(false);
+    		options[1].setVisible(false);
+    		options[2].setVisible(true);
+    		options[3].setVisible(true);
+    		options[4].setVisible(true);
+    		options[5].setVisible(true);
+    		
+    	} 
+    	else if (buttonName.equals("Add Patient")) {      
+    		new addPatientGUI("Patient Survey", 600, 930);
+    	} 
+    	else if (buttonName.equals("Remove Patient")) {      
+    		new removePatientGUI("Remove Patient", 600, 300);
+    	} 
+    	else if (buttonName.equals("Edit Existing Patient")) {      
+    		new editPatientGUI("Edit Existing Patient", 600, 980);
+    	} 
+    	else if (buttonName.equals("Return to Main Menu")) {      
+    		options[0].setVisible(true);
+    		options[1].setVisible(true);
+    		options[2].setVisible(false);
+    		options[3].setVisible(false);
+    		options[4].setVisible(false);
+    		options[5].setVisible(false);
     	} 
 	}
 }
