c06a616c73f0d4164746272a217416abe22cc733
Fabio <71391866+Fabio858@users.noreply.github.com>
2024-12-15 22:11:45 -0500

    Update editPatientGUI.java

--- editPatientGUI.java
+++ editPatientGUI.java
@@ -1,40 +1,44 @@
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 import javax.swing.*;
 public class editPatientGUI extends JFrame implements ActionListener{
-	JPanel panel[] = new JPanel[29]; 
-	JTextField adminInput[] = new JTextField[13];
-	JTextArea adminInstructions[] = new JTextArea[15];
-	String[] instructions = new String[15];
-	public editPatientGUI(String title, int height, int width) {
+	JPanel panel[] = new JPanel[26]; 
+	JTextField adminInput[] = new JTextField[12];
+	JTextArea adminInstructions[] = new JTextArea[14];
+	String[] instructions = new String[13];
+	patientForm sleepForm3;
+	
+	public editPatientGUI(String title, int height, int width, patientForm sleepForm2) {
+		sleepForm3 = sleepForm2;
 		setTitle(title);
 		setSize(height, width);
 		setLocation(400, 400);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		Container contentPane = this.getContentPane();
-		contentPane.setLayout(new GridLayout(30, 1));
+		contentPane.setLayout(new GridLayout(26, 1));
 		//instructions for the JTextAreas
-		instructions[0]= "Enter the ID of the patient to be edited.";
-		instructions[1]= "Gender";
-		instructions[2]= "Age";
-		instructions[3]= "Occupation";
-		instructions[4]= "Sleep Duration";
-		instructions[5]= "Quality of sleep";
-		instructions[6]= "Physical activity (minutes per day)";
-		instructions[7]= "Stress level on a scale of 1-10";
-		instructions[8]= "BMI estimation (normal, overweight, obese)";
-		instructions[9]= "Blood Pressure";
-		instructions[10]= "Resting Heart Rate";
-		instructions[11]= "Daily Steps";
-		instructions[12]= "Presence of sleep disorder (Y/N)";
-		instructions[13]= "Click on the Complete edit button once all patient data is ready to be entered into the database.";
+		instructions[0] = "Enter the ID of the patient to be edited.";
+		instructions[1] = "Gender";
+		instructions[2] = "Age";
+		instructions[3] = "Sleep Duration in hours";
+		instructions[4] = "Quality of sleep on a scale of 1-10";
+		instructions[5] = "Physical activity (minutes per day)";
+		instructions[6] = "Stress level on a scale of 1-10";
+		instructions[7] = "BMI estimation (normal, overweight, obese)";
+		instructions[8] = "Blood Pressure";
+		instructions[9] = "Resting Heart Rate";
+		instructions[10] = "Daily Steps";
+		instructions[11] = "Presence of sleep disorder (Y/N)";
+		instructions[12] = "Click on the Complete edit button once all patient data is ready to be entered into the database.";
 		//put panels onto the frame
 		for (int i = 0; i < panel.length; i++) {
 			panel[i] = new JPanel();
 			contentPane.add(panel[i]);
 		}
 		//put fields & inputs onto each individual panel for panels that are = to or greater than 3
-		for (int i = 0; i < 13; i++) {
+		for (int i = 0; i < 12; i++) {
 			adminInstructions[i] = new JTextArea();
 			adminInstructions[i].append(instructions[i]);
 			adminInstructions[i].setEditable(false);
@@ -50,7 +54,7 @@ public class editPatientGUI extends JFrame implements ActionListener{
 		panel[0].add(adminInstructions[0]);
 		panel[1].add(adminInput[0]);
 		panel[2].add(search);			
-		panel[28].add(finish);
+		panel[25].add(finish);
 		setVisible(true);
 		for (int i = 3; i < (panel.length - 1); i++) { 
 			panel[i].setVisible(false); 
@@ -103,7 +107,7 @@ public class editPatientGUI extends JFrame implements ActionListener{
 				try {
 					int patientIDInput1 = Integer.parseInt(adminInput[0].getText());
 					if (true) {
-						for (int i = 3; i < (panel.length - 1); i++) { 
+						for (int i = 3; i < (panel.length); i++) { 
 							panel[i].setVisible(true); 
 						}
 						panel[0].setVisible(false);
