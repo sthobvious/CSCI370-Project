42867c0651a7bbb683512afa78052c0b49a455ab
Fabio <71391866+Fabio858@users.noreply.github.com>
2024-12-15 22:13:45 -0500

    Update mainProgram.java

--- mainProgram.java
+++ mainProgram.java
@@ -1,6 +1,35 @@
 import java.io.*;
+import java.util.*;
 public class mainProgram {
-	public static void main(String[] args) throws IOException, DataNotFoundException{
-		new mainGUI("Sleep Disorder Detector", 600, 600);
+	public static void main(String[] args) throws IOException{
+		ArrayList<patient> patientStorage1 = new ArrayList<patient>();
+		try {
+	    	File patientFile = new File("Sleep_health_and_lifestyle_dataset.csv");
+	    	Scanner csvReader = new Scanner(patientFile);
+	    	
+	    	while (csvReader.hasNextLine()) {
+	    		int dataToEnterSlotNumber = 0;
+	    		String[] dataToEnter = new String[13];
+	    		String data = csvReader.nextLine();
+	    		StringTokenizer tokenizer = new StringTokenizer(data, ",/");
+	    		
+	            while (tokenizer.hasMoreTokens()) {
+	                dataToEnter[dataToEnterSlotNumber] = tokenizer.nextToken();
+	                if (dataToEnterSlotNumber == (dataToEnter.length - 1)) {
+	                	patient temporary = new patient(dataToEnter);
+	                	if (temporary.checkData() < 2) {
+	                		patientStorage1.add(temporary);
+	                	}
+	                }
+	                dataToEnterSlotNumber++;
+	            }
+	    	}
+	    	csvReader.close();
+	    }
+	    catch (FileNotFoundException e) {
+		    System.out.println("CSV File could not be found.");
+	    }
+		patientForm sleepForm = new patientForm(patientStorage1, 100);
+		new mainGUI("Sleep Disorder Detector", 600, 600, sleepForm);
 	}
 }
